a2327ed2bc6e2d67edd2707ca1075930dec047c7
Omar Mashaal <omar@teacups.io>
2022-05-09 13:23:54 +1000

    2.0 test

---
+++ devServer.ts
@@ -0,0 +1,3 @@
+import ultra from "https://raw.githubusercontent.com/exhibitionist-digital/ultra/v2-prepare/devServer.ts";
+
+ultra({ server: "./server.ts" });
--- server.ts
+++ server.ts
@@ -1,3 +1,3 @@
-import ultra from "https://deno.land/x/ultra@v1.0.1/server.ts";
+import ultra from "https://raw.githubusercontent.com/exhibitionist-digital/ultra/v2-prepare/server.ts";
 
 await ultra();
